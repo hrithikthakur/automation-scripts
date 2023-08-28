c8a13784368389aa5a5131437f9d9d607fe784b0
Hrithik Thakur <71100616+hrithikthakur@users.noreply.github.com>
2023-08-28 18:59:34 +0100

    introduced repo url as argument

--- privately_fork_a_public_repo.sh
+++ privately_fork_a_public_repo.sh
@@ -1,25 +1,34 @@
 #! /usr/local/env/bin/ bash
-cd ~/Code        #path to the default directory
-USERNAME="hrithikthakur"  #your github username
 
-REPO_URL="https://github.com/octocat/Hello-World/tree/octocat-patch-1"  #can be https or ssh
+# Make sure an argument is provided
+if [[ -z $1 ]]; then
+    echo "Usage: $0 <repository_url>"
+    exit 1
+fi
 
-git clone $REPO_URL #the cloned repo will have the same name as the repo name below.
+cd ~/Code
+USERNAME="hrithikthakur"    #your github username
+
+REPO_URL="$1"           
+
+# Clones the repository
+git clone $REPO_URL
 
 # Checks if the URL starts with "git@" (SSH) or "https://" (HTTPS)
 if [[ $REPO_URL == git@* ]]; then
     REPO_NAME=$(basename "$REPO_URL" .git)
-    echo "This is SSH"
 elif [[ $REPO_URL == https://* ]]; then
     # Remove protocol and trailing ".git" extension
     REPO_NAME=$(basename "$REPO_URL" .git)
     # Extract the repository name from the path
     REPO_NAME=$(basename "$REPO_NAME")
-    echo "This is HTTPS"
+else
+    echo "Unrecognized repository URL format"
+    exit 1
 fi
 
+# Creates a new remote repository
 gh repo create "$REPO_NAME" --private
-
 
 # Gets the URL of the newly created remote repository
 if [[ $REPO_URL == git@* ]]; then
@@ -35,8 +44,5 @@ git remote add origin $REMOTE_REPO_URL
 # Gets the name of the default branch
 DEFAULT_BRANCH=$(git symbolic-ref --short HEAD)
 
-#pushes it to the remote
+# Pushes to the remote
 git push --set-upstream origin $DEFAULT_BRANCH
-
-
-
