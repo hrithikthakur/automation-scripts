bed1b3782f2290c747ebf1a15209c29dd788f869
Hrithik Thakur <71100616+hrithikthakur@users.noreply.github.com>
2023-08-28 18:28:32 +0100

    basic version working

--- privately_fork_a_public_repo.sh
+++ privately_fork_a_public_repo.sh
@@ -1,11 +1,22 @@
 #! /usr/local/env/bin/ bash
 cd ~/Code
-git clone git@github.com:hrithikthakur/personal-website.git #ssh link
-REPO_NAME="first-handshake" #somehow extract repo name from the link?
-gh repo create $REPO_NAME --private --license mit
+USERNAME="hrithikthakur"
+SSH_URL="git@github.com:darkleaf/must_read.git"
+git clone $SSH_URL #the cloned repo will have the same name as the repo name below.
+
+# Extract the repository name from the SSH URL
+REPO_NAME=$(basename "$SSH_URL" .git)
+
+gh repo create "${REPO_NAME}" --private
+# Get the SSH URL of the newly created repository
+SSH_URL=$(gh repo view $USERNAME/$REPO_NAME --json sshUrl --jq .sshUrl)
+
+HTTPS_URL=$(gh repo view $USERNAME/$REPO_NAME --json url --jq .url)
+
 cd $REPO_NAME
 git remote remove origin
-git push --set-upstream origin main
+git remote add origin $SSH_URL
+git push --set-upstream origin master
 
 
 
